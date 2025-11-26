28b800a46a4a1b89bfca98fa75ed1be4fb80b15c
SteveMCWin <stevp003@gmail.com>
2025-11-26 22:32:54 +0100

    left some comments in the database.py which holds pretty much all of the logic

--- models/database.py
+++ models/database.py
@@ -3,16 +3,34 @@ from models import student, canteen, reservation
 
 
 class DB:
+    # all created students: key is id: int, value is student class
     students: dict
+    # all created canteens: key is id: int, value is canteen class
     canteens: dict
+    # all created reservations: key is id: int, value is reservation class
     reservations: dict
+    # holds num of reservations for each canteen. the key is the id of the canteen
+    # canteen_capacities[canteen_id] is another dict, where the key is the string
+    # representation of the date and time
+    # canteen_capacities[canteen_id][datetime_str] is how many people have reserved
+    # a spot
     canteen_capacities: dict
+    # dict of lists. dist key is canteen_id, list elements are
+    # ids of the reservations
+    # used for easier deletion of the reservations once a canteen is deleted
     canteen_reservations: dict
+    # keeps track of a students reservations. key is student_id
+    # value is another dict which has a key that is a string representation
+    # of the time and date 
+    # so if student_reservations[st_id][datetime_str] exists
+    # then the student has a reservation at that time-point
     student_reservations: dict
+    # these keep track of ids so ids are unique
     next_student_id: int
     next_canteen_id: int
     next_reservation_id: int
 
+    # keep track of data that needs to be unique
     emails: set
     canteen_locations: set
     canteen_names: set
@@ -41,7 +59,7 @@ class DB:
         self.emails.add(s.email)
 
         self.next_student_id += 1
-
+        # init dict that keeps track of reservations a student makes
         self.student_reservations[s.id] = {}
 
     def retrieve_student(self, id: int):
@@ -75,9 +93,6 @@ class DB:
             raise ValueError(
                 "There is already a canteen at location {}".format(ct.location))
 
-        # if len(ct.workingHours) != 3:
-        #     raise ValueError("Canteen must have three meals")
-
         ct.id = self.next_canteen_id
         self.canteens[ct.id] = ct
 
@@ -104,10 +119,11 @@ class DB:
         if not (ct.id in self.canteens):
             raise ValueError("There is no canteen with id {}".format(ct.id))
 
+        # make sure that if location changes, we stop keeping track of it
         if ct.location != self.canteens[ct.id].location:
             self.canteen_locations.remove(self.canteens[ct.id].location)
             self.canteen_locations.add(ct.location)
-
+        # same goes for the name
         if ct.name != self.canteens[ct.id].name:
             self.canteen_names.remove(self.canteens[ct.id].name)
             self.canteen_names.add(ct.name)
@@ -122,24 +138,27 @@ class DB:
         if not (ct_id in self.canteens):
             raise ValueError("There is no canteen with id {}".format(ct_id))
 
+        # remember to delete all reservations if we delete the canteen
         for r_id in self.canteen_reservations[ct_id]:
             self.delete_reservation(r_id, student_id)
         self.canteen_locations.remove(self.canteens[ct_id].location)
         self.canteen_names.remove(self.canteens[ct_id].name)
         self.canteens.pop(ct_id)
         self.canteen_capacities.pop(ct_id)
-        # self.delete_all_reservations_in_canteen(ct_id)
 
     def isDateInThePast(self, d: dt.date, t: dt.time):
         dt_reservation = dt.datetime.combine(d, t)
         return dt_reservation < dt.datetime.now()
 
+    # the naming is a bit misleading, here we are just updating
+    # the number of reservations, not linking them with canteens
     def addReservationToCanteen(self, ct_id: int, key: str):
         if key in self.canteen_capacities[ct_id]:
             self.canteen_capacities[ct_id][key] += 1
         else:
             self.canteen_capacities[ct_id][key] = 1
 
+    # also a bit misleading
     def deleteReservationFromCanteen(self, ct_id: int, key: str):
         if not (ct_id in self.canteen_capacities):
             raise ValueError(
@@ -158,6 +177,10 @@ class DB:
         self.canteen_reservations[ct_id].append(r.id)
         print(self.canteen_reservations[ct_id])
 
+        # note that the way I've designed this is that we only see
+        # time in increments of 30 minutes. so if a student
+        # will be in the canteen for more than 30 mintes, we have
+        # to update the next time-point as well
         if r.duration == 60:
             dt_combined = dt.datetime.combine(r.date, r.time)
             dt_plus_30 = dt_combined + dt.timedelta(minutes=30)
@@ -179,6 +202,9 @@ class DB:
                 dt_plus_30.time().strftime('%H:%M')}"
             self.deleteReservationFromCanteen(ct_id, key2)
 
+    # a bit misleading, we aren't linking the reservation to the
+    # student, we are just saying "this student has a reservation
+    # at this date and time"
     def addReservationToStudent(self, student_id: int, key: str):
         if not (student_id in self.students):
             raise ValueError(
@@ -194,6 +220,7 @@ class DB:
                 "Student with id {} doesn't have a reservation at {}".format(student_id, key))
         self.student_reservations[student_id].pop(key)
 
+    # same as canteen reservations, we are looking at time in 30 minute increments
     def handleNewStudentReservation(self, student_id: int, r: reservation.Reservation):
         key1 = f"{r.date.isoformat()}|{r.time.strftime('%H:%M')}"
         self.addReservationToStudent(student_id, key1)
@@ -216,6 +243,8 @@ class DB:
                 dt_plus_30.time().strftime('%H:%M')}"
             self.deleteReservationFromStudent(student_id, key2)
 
+    # returns true if the student has another reservation
+    # at the same date and time as the passed reservation
     def doesReservationOverlap(self, r: reservation.Reservation):
         key1 = f"{r.date.isoformat()}|{r.time.strftime('%H:%M')}"
         if key1 in self.student_reservations[r.studentId]:
@@ -230,6 +259,8 @@ class DB:
 
         return False
 
+    # returns true if specified time and duration fits into
+    # the working hours (slots) of a canteen
     def isValidMealTime(self, r: reservation.Reservation):
         r_start_dt = dt.datetime.combine(r.date, r.time)
         r_end_dt = r_start_dt + dt.timedelta(minutes=r.duration)
@@ -243,6 +274,8 @@ class DB:
 
         return False
 
+    # returns true if max capacity reached for time-point or
+    # time-points specified in reservation
     def isCanteenFull(self, r: reservation.Reservation):
         ct = self.retrieve_canteen(r.canteenId)
         key1 = f"{r.date.isoformat()}|{r.time.strftime('%H:%M')}"
@@ -317,6 +350,9 @@ class DB:
 
         return r
 
+    # return the name of the meal (e.g. 'dorucak') based on the
+    # time. so if 'dorucak' lasts from 09:00 to 10:00, the function
+    # returns 'dorucak' when you pass in 09:30
     def getCanteenMealName(self, ct_id: int, t: dt.time):
         ct = self.retrieve_canteen(ct_id)
         for m in ct.workingHours:
@@ -325,6 +361,8 @@ class DB:
 
         return ""
 
+    # returns remaining capacities for a canteen for the time and date intervals specified
+    # incrementing the time from 'startTime' to 'dateTime' by 'duration' minutes
     def get_canteen_cap_status(self, ct_id: int, startDate: dt.date, endDate: dt.date, startTime: dt.time, endTime: dt.time, duration: int):
         if duration != 30 and duration != 60:
             raise ValueError("The duration must be either 30 or 60 (minutes)")
@@ -359,6 +397,7 @@ class DB:
 
         return res
 
+    # runs the above function for all canteens currently stored in db
     def get_all_canteens_cap_status(self, startDate: dt.date, endDate: dt.date, startTime: dt.time, endTime: dt.time, duration: int):
         res = []
         for ct_id in self.canteens:
